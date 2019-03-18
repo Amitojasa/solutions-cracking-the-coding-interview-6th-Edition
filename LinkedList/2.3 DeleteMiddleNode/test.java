0b1b22fc0e7d46056c845ba9f2853edb8ad954c8
Amitojasa <amitojvmc@gmail.com>
2019-03-18 16:54:54 +0530

    LinkedList

---
+++ LinkedList/2.3 DeleteMiddleNode/test.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class node{
+	int data;
+	node next=null;
+	
+	node(int data){
+		this.data=data;
+	}
+	
+	public void add(int data){
+		node end = new node(data);
+		node n=this;
+		while(n.next!=null){
+			n=n.next;
+		}
+		n.next=end;
+	}
+	
+	public void show(){
+		node n=this;
+		while(n!=null){
+			System.out.print(n.data+" ");
+			n=n.next;
+		}
+		System.out.println();
+	}
+	
+	public void delNode(int val){
+		
+		node prev=this;
+		node n=prev.next;
+		while(prev.next!=null){
+			
+			if(n.data==val){
+				prev.next=n.next;
+			}
+			prev=prev.next;
+			n=n.next;
+			
+			
+		}
+	}
+}
+class test{
+	public static void main(String args[]){
+		
+		node n=new node(0);
+		n.add(10);
+		n.add(20);
+		n.add(30);
+		n.add(40);
+		n.show();
+		n.delNode(20);
+		
+		System.out.println("After deleting");
+		
+		n.show();
+	}
+}
